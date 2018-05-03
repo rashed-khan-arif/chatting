a12b99f048715a25a824d5c51dedcd2faab24ec8
Rashed Khan Arif <arifkhan0713@gmail.com>
2018-05-04 00:46:22 +0600

    applied style

---
+++ src/main/java/com/project/chatting/servlet/Home.java
@@ -0,0 +1,16 @@
+package com.project.chatting.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/home")
+public class Home extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("Home servlet !");
+    }
+}
